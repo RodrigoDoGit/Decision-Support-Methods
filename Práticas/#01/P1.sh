320f52d9468cd1de54c30f69fb4074f8a4b38563
Rodrigo Emídio <94897828+RodrigoDoGit@users.noreply.github.com>
2023-04-30 19:25:48 +0100

    Delete P1.sh

--- Práticas/#01/P1.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..7}
-do
-    echo -e "Exercício $i:\n" 
-    echo -e "Resolução com AMPL:\n"
-    ampl ex$i.run
-    echo -e "Resolução com GLPK:\n"
-    glpsol --math ex$i.mod -o ex$i.sol
-    echo -e "\n"
-done
